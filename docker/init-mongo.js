346a8a2caa9580dd0d96d75cfb7175f5ffe5b9bd
Andrew Kew <andrew@quadcorps.co.uk>
2024-12-04 11:36:33 +0000

    Adding an API Stub using wiremock. Creating a new mongo collection with init data, adding the wiremock API stubs

--- docker/init-mongo.js
+++ docker/init-mongo.js
@@ -14,3 +14,12 @@ db.createUser({
 });
 
 db.createCollection('accounts');
+db.createCollection('customers');
+
+db.customers.insert([
+    {"_id" : "1","title" : "Mr","firstname" : "John","surname" : "Doe"},
+    {"_id" : "2","title" : "Mrs","firstname" : "Jane","surname" : "Doe"},
+    {"_id" : "3","title" : "Dr","firstname" : "Theodor","surname" : "Seuss"},
+    {"_id" : "4","title" : "Miss","firstname" : "Janie","surname" : "Doe"},
+    {"_id" : "5","title" : "Mr","firstname" : "Richard","surname" : "Roe"},
+]);
